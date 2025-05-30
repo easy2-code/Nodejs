9db28a1745a7c4f007daa663176dcc19fed84916
Tayyab Khan <97843826+easy2-code@users.noreply.github.com>
2025-05-30 22:43:08 +0500

    Send Status Code in response....

--- 10 Status Code/server.js
+++ 10 Status Code/server.js
@@ -2,7 +2,7 @@ const http = require("http");
 const fs = require("fs");
 
 const server = http.createServer((req, res) => {
-  console.log(req.url, req.method);
+  console.log(req.url, req.method); // log the request URL and method in terminal
 
   // set header content type
   res.setHeader("Content-Type", "text/html");
